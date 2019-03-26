db4a5cdfff1cea124239bd68c39cb0aa88ec885f
Ripostory <ripostory@gmail.com>
2019-03-25 21:45:50 -0700

    Added pulling server side data

---
+++ frontEnd/airline.js
@@ -0,0 +1,18 @@
+var xhttp = new XMLHttpRequest();
+
+// Run on document loaded
+$( document ).ready(function() {
+    console.log( "ready!" );
+    getTopCountry();
+});
+
+function getTopCountry() {
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      $("#gate-country").html(response.country);
+    }
+  };
+  xhttp.open("GET", "https://api.ripostory.com/air/gate/top-country", true);
+  xhttp.send();
+}
