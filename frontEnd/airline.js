87c9ad2f9f2ff9ab9b58f3103eda7b96cb13c846
Ripostory <ripostory@gmail.com>
2019-03-27 19:29:24 -0700

    Top cities now pull data from server

--- frontEnd/airline.js
+++ frontEnd/airline.js
@@ -17,6 +17,37 @@ function getTopCountry() {
   xhttp.send();
 }
 
-function getTopCities(cityCount, isIncoming) {
-  //TODO implement
+function getTopCities() {
+  //definition on retrieve data
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+
+      //add all entries into table
+      var rank = 1;
+      var finalHTML = "";
+      response.cities.forEach( function (entry) {
+        finalHTML += addCityEntry(rank, entry);
+        rank++;
+      });
+      $("#table-topcity").html(finalHTML);
+    }
+  };
+
+  var form = $("#form-topcity");
+  if (form.find('input[name="count"]').val() > 0 && form.find('input[name="count"]').val() != "") {
+    //clear data
+    $("#table-topcity").html("");
+
+    //retrieve data from server
+    xhttp.open("GET", "https://api.ripostory.com/air/gate/top-city?" + form.serialize(), true);
+    xhttp.send();
+  }
+
+  return false;
+}
+
+function addCityEntry(rank, entry) {
+  return '<tr><th scope="row">' + rank + '</th><td>' + entry.name +
+    '</td><td>' + entry.incoming + '</td><td>' + entry.outgoing +'</td></tr>';
 }
